c49134687b0735e4d69ffa8d54fe7b7c71abbdd7
cjsolis <cjelizondo12@gmail.com>
2023-04-28 10:41:02 -0500

    added helper file functions

---
+++ Develop/routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+//Import modular routers for /notes
+
+const notesRouter = require('.notes')
+
+
+const app = express();
+
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/notes', notesRouter)
+
+
+module.exports = app;
